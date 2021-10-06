b540b01db2f1de31c9727425a8f9228fb5fd89c7
niubility000 <76441520+niubility000@users.noreply.github.com>
2021-10-06 17:23:37 +0800

    Add files via upload

--- frontend/src/components/files/ExtendedImage.vue
+++ frontend/src/components/files/ExtendedImage.vue
@@ -4,9 +4,10 @@
     ref="container"
     @touchstart="touchStart"
     @touchmove="touchMove"
+    @touchend="touchEnd"
     @dblclick="zoomAuto"
     @mousedown="mousedownStart"
-    @mousemove="mouseMove"
+    @mousemove="mouseMove"    
     @mouseup="mouseUp"
     @wheel="wheelMove"
   >
@@ -25,9 +26,10 @@ import UTIF from "utif";
 export default {
   props: {
     src: String,
+    angle: Number,
     moveDisabledTime: {
       type: Number,
-      default: () => 200,
+      default: () => 50,
     },
     classList: {
       type: Array,
@@ -35,7 +37,7 @@ export default {
     },
     zoomStep: {
       type: Number,
-      default: () => 0.25,
+      default: () => 0.18,
     },
   },
   data() {
@@ -45,7 +47,9 @@ export default {
       lastY: null,
       inDrag: false,
       touches: 0,
+      scope: "",
       lastTouchDistance: 0,
+      navX: 0,
       moveDisabled: false,
       disabledTimer: null,
       imageLoaded: false,
@@ -53,8 +57,9 @@ export default {
         center: { x: 0, y: 0 },
         relative: { x: 0, y: 0 },
       },
-      maxScale: 4,
-      minScale: 0.25,
+      maxScale: 8,
+      minScale: 1,
+      oScale: 0,
     };
   },
   mounted() {
@@ -83,10 +88,27 @@ export default {
         this.$refs.imgex.src = this.src;
       }
 
-      this.scale = 1;
-      this.setZoom();
+
       this.setCenter();
     },
+    angle: function () {
+      
+      this.setCenter()
+    }
+  },
+  computed: {
+    imgHeight() {
+      let img = this.$refs.imgex
+      // slow version
+      // return img.clientHeight * Math.cos(this.angle * Math.PI) + img.clientWidth * Math.sin(this.angle * Math.PI)
+      return !(this.angle % 180) ? img.clientHeight : img.clientWidth
+    },
+    imgWidth() {
+      let img = this.$refs.imgex
+      // slow version
+      // return img.clientWidth * Math.cos(this.angle * Math.PI) + img.clientHeight * Math.sin(this.angle * Math.PI)
+      return !(this.angle % 180) ? img.clientWidth : img.clientHeight
+    }
   },
   methods: {
     // Modified from UTIF.replaceIMG
@@ -103,6 +125,46 @@ export default {
       xhr.send();
       return true;
     },
+    
+    fit() {
+      const wScale = window.innerWidth / this.imgWidth
+      const hScale = window.innerHeight / this.imgHeight
+    
+      this.oScale = Math.min(wScale, hScale)
+      this.scale = this.oScale
+      this.minScale = this.oScale
+      this.maxScale = 8*this.oScale
+      this.setZoom()
+    },
+    refit() {
+      const target = this.fitScreenTarget()
+      this.doMove(target[0], target[1])
+    },
+    fitScreenTarget() {
+      if (this.scale <= this.minScale) {
+        let style = this.$refs.imgex.style
+        let posX = this.pxStringToNumber(style.left)
+        let posY = this.pxStringToNumber(style.top)
+        return [this.position.center.x - posX, this.position.center.y - posY]
+      }
+      else {
+        let img = this.$refs.imgex
+        const rect = img.getBoundingClientRect()
+        const width = window.innerWidth
+        const height = window.innerHeight
+        let x = 0,y = 0
+        // left out of viewport
+        if (rect.left < 0 && rect.right < width) x = Math.min(-rect.left, width - rect.right)
+        // right out of viewport
+        else if (rect.left > 0 && rect.right > width) x = Math.max(-rect.left, width - rect.right)
+        // top out of viewport
+        if (rect.top < 0 && rect.bottom < height) y = Math.min(-rect.top, height - rect.bottom)
+        // bottom out of viewport
+        else if (rect.top > 0 && rect.bottom > height) y = Math.max(-rect.top, height - rect.bottom)
+      return [x,y]
+      }
+    },
+    
     onLoad() {
       let img = this.$refs.imgex;
 
@@ -131,10 +193,11 @@ export default {
       const fullScale = realSize / displaySize;
 
       // Full size plus additional zoom
-      this.maxScale = fullScale + 4;
+       this.maxScale = fullScale *14;
     },
     onMouseUp() {
       this.inDrag = false;
+      this.refit();
     },
     onResize: throttle(function () {
       if (this.imageLoaded) {
@@ -142,6 +205,7 @@ export default {
         this.doMove(this.position.relative.x, this.position.relative.y);
       }
     }, 100),
+
     setCenter() {
       let container = this.$refs.container;
       let img = this.$refs.imgex;
@@ -155,6 +219,7 @@ export default {
 
       img.style.left = this.position.center.x + "px";
       img.style.top = this.position.center.y + "px";
+      this.fit();
     },
     mousedownStart(event) {
       this.lastX = null;
@@ -164,6 +229,7 @@ export default {
     },
     mouseMove(event) {
       if (!this.inDrag) return;
+      this.navX = event.movementX;
       this.doMove(event.movementX, event.movementY);
       event.preventDefault();
     },
@@ -187,16 +253,20 @@ export default {
       event.preventDefault();
     },
     zoomAuto(event) {
-      switch (this.scale) {
-        case 1:
-          this.scale = 2;
+      if(this.scale<2*this.oScale) {this.scope = "<2";}
+      if(this.scale>=2*this.oScale && this.scale<4*this.oScale ) {this.scope = "2to4";}
+      if(this.scale>=4*this.oScale) {this.scope = ">4";}
+      
+      switch (this.scope) {
+        case "<2":
+          this.scale = 2*this.oScale;
           break;
-        case 2:
-          this.scale = 4;
+        case "2to4":
+          this.scale = 4*this.oScale;
           break;
         default:
-        case 4:
-          this.scale = 1;
+        case ">4":
+          this.scale = this.oScale;
           this.setCenter();
           break;
       }
@@ -228,9 +298,14 @@ export default {
           this.lastTouchDistance = touchDistance;
           return;
         }
-        this.scale += (touchDistance - this.lastTouchDistance) / step;
-        this.lastTouchDistance = touchDistance;
-        this.setZoom();
+	
+	      if((touchDistance > this.lastTouchDistance) ||this.scale > this.oScale){
+	
+          this.scale += (touchDistance - this.lastTouchDistance) / step
+          this.lastTouchDistance = touchDistance
+          this.setZoom()
+        } 
+	
       } else if (event.targetTouches.length === 1) {
         if (this.moveDisabled) return;
         let x = event.targetTouches[0].pageX - this.lastX;
@@ -241,33 +316,61 @@ export default {
         this.doMove(x, y);
       }
     },
-    doMove(x, y) {
-      let style = this.$refs.imgex.style;
-      let posX = this.pxStringToNumber(style.left) + x;
-      let posY = this.pxStringToNumber(style.top) + y;
-
-      style.left = posX + "px";
-      style.top = posY + "px";
-
-      this.position.relative.x = Math.abs(this.position.center.x - posX);
-      this.position.relative.y = Math.abs(this.position.center.y - posY);
-
-      if (posX < this.position.center.x) {
-        this.position.relative.x = this.position.relative.x * -1;
+    touchEnd(event) {
+      if (event.targetTouches.length === 0) {
+        this.refit()
       }
+    },
+    doMove(x, y) {
+      if(this.scale!==this.oScale){
+        let style = this.$refs.imgex.style;
+        let posX = this.pxStringToNumber(style.left) + x;
+        let posY = this.pxStringToNumber(style.top) + y;
 
-      if (posY < this.position.center.y) {
-        this.position.relative.y = this.position.relative.y * -1;
+        style.left = posX + "px";
+        style.top = posY + "px";
+
+        this.position.relative.x = Math.abs(this.position.center.x - posX);
+        this.position.relative.y = Math.abs(this.position.center.y - posY);
+
+        if (posX < this.position.center.x) {
+          this.position.relative.x = this.position.relative.x * -1;
+        }
+
+        if (posY < this.position.center.y) {
+          this.position.relative.y = this.position.relative.y * -1;
+        }
+      }
+      
+      
+      if(this.scale===this.oScale){
+
+         let style = this.$refs.imgex.style;
+         let posX = this.pxStringToNumber(style.left) + x;
+
+          style.left = posX + "px";
+
+          this.position.relative.x = Math.abs(this.position.center.x - posX);
+
+          if (posX < this.position.center.x) {
+            this.position.relative.x = this.position.relative.x * -1;
+          }
+	
       }
     },
     wheelMove(event) {
       this.scale += -Math.sign(event.deltaY) * this.zoomStep;
       this.setZoom();
     },
+    
     setZoom() {
       this.scale = this.scale < this.minScale ? this.minScale : this.scale;
       this.scale = this.scale > this.maxScale ? this.maxScale : this.scale;
-      this.$refs.imgex.style.transform = `scale(${this.scale})`;
+     // this.$refs.imgex.style.transition ="0.8s";  对入场，拖动，缩放都有效了。希望只对入场有效，怎么做？在css里加会对入场和缩放有效，对拖动无效，好一些。
+      this.$refs.imgex.style.transform = `scale(${this.scale}) rotate(${this.angle}deg)`;
+
+      this.refit()
+      event.preventDefault();
     },
     pxStringToNumber(style) {
       return +style.replace("px", "");
