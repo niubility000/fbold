9b5a5a028f974cc7ecef660990f17ac57700d075
niubility000 <76441520+niubility000@users.noreply.github.com>
2021-10-06 17:30:23 +0800

    Add files via upload

--- version/version.go
+++ version/version.go
@@ -2,7 +2,7 @@ package version
 
 var (
 	// Version is the current File Browser version.
-	Version = "(untracked)"
+	Version = "2.17.2-update-modified"
 	// CommitSHA is the commmit sha.
 	CommitSHA = "(unknown)"
 )
